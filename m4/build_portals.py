ebe614f2ee75c073f595f4174e7a55d448b8cad7
Nicholas Russo <njrusmc@gmail.com>
2020-02-04 17:52:30 -0500

    minor cleanup of meraki

--- m4/build_portals.py
+++ m4/build_portals.py
@@ -8,10 +8,11 @@ external captive portal (excap) settings on Meraki SSIDs.
 
 import os
 import json
+import sys
 from meraki_helpers import get_network_id, req
 
 
-def main(org_name, net_name):
+def main(org_name, net_name, file_name):
     """
     Execution begins here.
     """
@@ -19,9 +20,12 @@ def main(org_name, net_name):
     # Find the network ID for our reserved instance (or personal org)
     net_id = get_network_id(net_name, org_name)
 
-    with open("add_portals.json", "r") as handle:
+    # Load in the portal dictionary from the specified file
+    with open(file_name, "r") as handle:
         portals = json.load(handle)
 
+    # Loop will only run once, but this allows us to put multiple
+    # portals in a single file for future batch processing
     for ssid_number, body in portals.items():
 
         # Assemble the SSID base URL and the HTTP PUT request payload
@@ -34,11 +38,16 @@ def main(org_name, net_name):
         # Debugging statement to check the updated SSID information
         # print(json.dumps(update_ssid.json(), indent=2))
 
-        # Issue the PUT request to update the splash page parameters if they exist
+        # Issue the PUT request to update splash page parameters if they exist
         if body["splash_body"]:
-            print(f"Update SSID {ssid_number} excap to {body['splash_body']['splashUrl']}")
+            print(
+                f"Update SSID {ssid_number} excap "
+                f"to {body['splash_body']['splashUrl']}"
+            )
             update_splash = req(
-                f"{ssid_base}/splashSettings", method="put", json=body["splash_body"]
+                f"{ssid_base}/splashSettings",
+                method="put",
+                json=body["splash_body"],
             )
 
         # Debugging statement to check the updated splash information
@@ -52,5 +61,16 @@ if __name__ == "__main__":
     # Get the network name from the env var; default to DevNet
     net = os.environ.get("MERAKI_NET_NAME", "DevNet Sandbox Always on READ ONLY")
 
+    # Ensure the user supplied a CLI argument to specify the portals file
+    if len(sys.argv) != 2:
+        print("usage: python build_portals.py <path_to_json_file>")
+        sys.exit(1)
+
+    # Ensure the supplied file exists
+    json_file = sys.argv[1]
+    if not os.path.exists(json_file):
+        print(f"file '{json_file}' does not exist")
+        sys.exit(2)
+
     # Pass in the org and network arguments into main()
-    main(org, net)
+    main(org, net, json_file)
